cf833b9b8dece77ce27c3fa6b03cb97ee9093623
Elad Karako <147068034+eladkarako@users.noreply.github.com>
2026-06-25 22:03:55 +0300

    cleanup and adding maintained last 3 recently activated tabs (at least one of them is the active one). helps to avoid discarding tab you potentially be come back to.

--- sw.js
+++ sw.js
@@ -1,32 +1,49 @@
 "use strict";
 
-const api        = chrome || browser || {runtime:{lastError:true}}
-     ,alarm_name = api.i18n.getMessage("alarm_name") || "alarm_to_discard_tabs"
-     ,alarm_info = {periodInMinutes : 1.0
-                   }
-     ,query_info = {active    : false
-                   ,audible   : false
-                   ,discarded : false
-                   ,frozen    : false
-                   }
-     ;
+/* - register a 1 minute alarm.
+ * - discard all tabs that are not recently activated, nor audible.
+ *
+ * - note. storing the last 3 IDs, of the most recently activated tabs. (IDs are relevant to current browser session. storage is kept for session as well).
+ */
 
+const api = chrome || browser || {runtime:{lastError:true}};
 
 const installed_handler = (details)=>{
   if("undefined" !== typeof api.runtime.lastError && null !== api.runtime.lastError){ return true; }
-  api.alarms.create(alarm_name, alarm_info)
-  .then(alarm=>{ 
-  }).catch(err=>{ 
+  api.alarms.create("alarm_to_discard_tabs", {periodInMinutes : 1.0})
+    .then(alarm=>{ 
+    })
+    .catch(err=>{ 
+    });
+};
+
+const add_id = async (id)=>{
+  let ids = ((await api.storage.session.get("ids")) || {"ids":[]})["ids"];
+  ids.unshift(id);
+  ids.length = 3;
+  return api.storage.session.set({"ids":ids});
+};
+
+const tab_activated_handler = (tab_active_info)=>{
+  if("undefined" !== typeof api.runtime.lastError && null !== api.runtime.lastError){ return true; }
+
+  add_id(tab_active_info.tabId)
+  .then(()=>{
+  })
+  .catch(err=>{
   });
 };
 
 const discard_single_tab = async (tab)=>{
   if(!tab.id){return true;}
+  const ids = ((await api.storage.session.get("ids")) || {"ids":[]}).ids;
+  const is_recently_activated = (-1 !== ids.indexOf(tab.id));
+  if(true === is_recently_activated){return true;}
   return api.tabs.discard(tab.id);
 };
 
 const discard_tabs = async ()=>{
-  let tabs = await api.tabs.query(query_info);
+  let tabs = await api.tabs.query({active:false, audible:false, discarded:false, WindowType:"normal"});
   const settled_results = await Promise.allSettled(tabs.map(tab=>(discard_single_tab(tab))));
   tabs = tabs.map((tab,index)=>{
     const o = {};
@@ -41,30 +58,26 @@ const all_alarms_handler = (alarm)=>{
   if("undefined" !== typeof api.runtime.lastError && null !== api.runtime.lastError){ return true; }
   if (!alarm || alarm_name !== alarm.name){ return true; }
 
-  discard_tabs().then(tabs=>{
-  }).catch(err=>{
+  discard_tabs()
+  .then(()=>{
+  })
+  .catch(err=>{
   });
 };
 
 queueMicrotask(()=>{
   api.runtime.onInstalled.addListener(installed_handler);
+  api.tabs.onActivated.addListener(tab_activated_handler);
   api.alarms.onAlarm.addListener(all_alarms_handler);
 });
 
-
-/* https://developer.chrome.com/docs/extensions/reference/api/i18n
- * https://developer.chrome.com/docs/extensions/reference/api/alarms
- * https://developer.chrome.com/docs/extensions/reference/api/tabs
- * https://developer.chrome.com/docs/extensions/reference/api/runtime
- * https://developer.chrome.com/docs/web-platform/page-lifecycle-api
- * https://developer.mozilla.org/en-US/docs/Web/API/Window/queueMicrotask
- *
- * notes:
- * - `api.runtime.lastError` - Chrome needs you to check it. doing so triggers via its getter a secondary system that marks "you have checked it" for the developer extension page. it is only available from within callbacks. when there is no error in Chromium based browsers the value will be undefined, but in Mozilla it will be null.
- * - `chrome || browser` will handle chromium and firefox based browsers.
- * - `api.alarms.get(alarm_name);` is not needed. creating alarm with same-name replaces any existing one.
- * - `queueMicrotask` may be removed. there aren't tiny tasks in here really.
- * - there are several empty .then and .catch for example `discard_tabs().then(tabs=>{` you can include console.log to view the states. it isn't really needed, it is just for debug, bug without console.log line in the code. I can confirm that chromium and edge respect the discard call and instead of waiting for the tab to sleep then discard it in about 5minute - 1 hour (demanding on available RAM and CPU), it changes state right away.
+/* notes:
+ * - `api.runtime.lastError`'s getter is consumed via callback to make chromium happy.
+ * - `const api = chrome || browser` for Firefox.
+ * - `api.alarms.get(alarm_name);` is not needed. creating an alarm with same-name replaces any existing one.
+ * - `queueMicrotask` to set hooks since its fast.
+ * - empty `.then` and `.catch` are for debug purposes. add console.log if needed.
+ * - maintaining array of the last 3 recently activated tabs, to avoid discarding them. the data is stored in session storage. tab IDs are not private information, just numbers relevant to order of tabs within current session. data is optional, just to improve user experience, there is a chance recent tabs will be reused more often.
  * licensed under MIT. https://github.com/eladkarako/webextension-speedup/issues/new  
  *
  * ░▒▓█■═▬─—▄▀
