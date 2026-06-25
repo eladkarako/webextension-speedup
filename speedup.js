2a7485a77328eab88f5dc5594a788bc74997e5bd
Elad Karako <147068034+eladkarako@users.noreply.github.com>
2026-06-25 22:29:41 +0300

    wrapping with (()=>{})(); since const variable tried to be re-defined.

--- speedup.js
+++ speedup.js
@@ -1,29 +1,30 @@
-"use strict";
+(()=>{
+  "use strict";
 
-const query = 'img,iframe,video,audio';
+  const query = 'img,iframe,video,audio';
 
-const action_per_element = async (element)=>{
-  element.loading = "lazy"; //supported for img,iframe and in some cases video.
-  element.preload = "none"; //supported in video and audio. does not matter for other nodes (has no setter).
-};
+  const action_per_element = async (element)=>{
+    element.loading = "lazy"; //supported for img,iframe and in some cases video.
+    element.preload = "none"; //supported in video and audio. does not matter for other nodes (has no setter).
+  };
 
-const action = async ()=>{
-  let elements = Array.from(document.querySelectorAll(query));
-  const settled_results = await Promise.allSettled(elements.map(element=>action_per_element(element)));
-  elements = elements.map((element,index)=>{
-    const o = {};
-    o.element = element;
-    o.settled_result = settled_results[index];
-    return o;
+  const action = async ()=>{
+    let elements = Array.from(document.querySelectorAll(query));
+    const settled_results = await Promise.allSettled(elements.map(element=>action_per_element(element)));
+    elements = elements.map((element,index)=>{
+      const o = {};
+      o.element = element;
+      o.settled_result = settled_results[index];
+      return o;
+    });
+    return elements;
+  };
+
+  action().then(elements=>{
+  }).catch(err=>{
   });
-  return elements;
-};
 
-action().then(elements=>{
-}).catch(err=>{
-});
-
-
+})();
 
 
 /* - code is triggered by manifest.json two times, no additional events. 
